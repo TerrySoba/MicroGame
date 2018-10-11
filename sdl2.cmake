de331c970f4b68501a352c1d7ddeceaf9d6fc89b
Unknown <torsten@stremlau.eu>
2018-10-11 02:02:08 +0200

    32bits

--- sdl2.cmake
+++ sdl2.cmake
@@ -2,12 +2,12 @@
 if (WIN32)
     set(INTERNAL_SDL2_DIR C:/SDL2-devel-2.0.8-VC/SDL2-2.0.8/)
     set(SDL2_LIBRARIES
-        ${INTERNAL_SDL2_DIR}/lib/x64/SDL2.lib
-        ${INTERNAL_SDL2_DIR}/lib/x64/SDL2main.lib
+        ${INTERNAL_SDL2_DIR}/lib/x86/SDL2.lib
+        ${INTERNAL_SDL2_DIR}/lib/x86/SDL2main.lib
         )
     set(SDL2_INCLUDE_DIR ${INTERNAL_SDL2_DIR}/include)
 
-    install(FILES ${INTERNAL_SDL2_DIR}/lib/x64/SDL2.dll
+    install(FILES ${INTERNAL_SDL2_DIR}/lib/x86/SDL2.dll
         DESTINATION bin)
 else (WIN32)
     find_package(SDL2 REQUIRED)
